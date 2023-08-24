58a98aee923626f31fe9aa71ab383148a4c2abd2
MohammedNayeemE <127304665+MohammedNayeemE@users.noreply.github.com>
2023-08-24 13:59:43 +0530

    readme updated , App.jsx updated

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # ThriveTogether
 journal has been updated with database feature.
 mood tracker page prototype has been created.
-routing has been updated
-next job is to add authentication using firebase.
-work in progress.
+routing has been updated(1).
+authentication is commited , lets wait for the result.
+
+next work is to see the current user state , make changes in the database accordingly .
+work in progress..
 
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import SignUp from "./signup";
 import Journal from "./journal";
 import { Container } from "react-bootstrap";
 import MoodTracker from "./mood";
+import Login from "./login.jsx";
 import {BrowserRouter as Router , Route , Routes} from 'react-router-dom';
 
 
@@ -13,6 +14,7 @@ function App(){
                 <Route path="/" element={<SignUp/>}/>
                 <Route path="/journal" element={<Journal/>}/>
                 <Route path="/mood" element={<MoodTracker/>}/>
+                <Route path="/login" element = {<Login/>}/>
             </Routes>
         </Router>
         </>

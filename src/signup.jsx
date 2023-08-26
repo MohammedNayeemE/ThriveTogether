c0a6a09dd62fe071b53ebd33082763624ffedbd4
MohammedNayeemE <127304665+MohammedNayeemE@users.noreply.github.com>
2023-08-26 16:55:52 +0530

    validation added.

--- src/signup.jsx
+++ src/signup.jsx
@@ -1,15 +1,20 @@
-import {Card , Form , Button} from 'react-bootstrap';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword , auth } from './firebaseconfig.js';
+import {Card , Form , Button, Container} from 'react-bootstrap';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword , auth } from '/firebaseconfig.js';
 import { useState } from 'react';
 import {useNavigate} from 'react-router-dom';
+import './custom.css';
 function SignUp(){
 const navigate = useNavigate();
     const [email , setEmail] = useState('');
     const [password , setPassword] = useState('');
+    const [cpassword , setCpassword] = useState('');
+     
 
+    let isInvalidpassword = password.length < 8;
+    let isemptyField = email.length ==0;
     const Submit = async (e) =>{
         e.preventDefault();
-
+        if(!(isemptyField && isInvalidpassword)){
         await createUserWithEmailAndPassword(auth , email , password)
         .then((userCredential) =>{
             const user = userCredential.user;
@@ -22,42 +27,62 @@ const navigate = useNavigate();
             console.log(error.code , error.message);
         })
     }
-
+    else{
+        alert('Working...')
+    }
+    };
     return(
         <>
-        <Card>
-            <Card.Body>
-                <h2 className='text-center mb-4'>Sign Up</h2>
-                <Form>
-                    <Form.Group id='email'>
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type='email' value = {email} onChange = {(e) => setEmail(e.target.value)}/>
-                    </Form.Group>
-                    <Form.Group id='password'>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type='password' value = {password} onChange = {(e) => setPassword(e.target.value)}/>
-                    </Form.Group>
-                    <Form.Group id='confirmpassword'>
-                        <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control type='password' value = {password}/>
-                    </Form.Group>
-                    <Button type='submit' className='w-100 mt-2' onClick = {Submit}>
-                        SignUp
-                    </Button>
-                </Form>
-            </Card.Body>
-        </Card>
-        <div className='w-100 text-center mt-2'>
-            Already Have an Account ?
-            <br/>
-               _________________
-               <Button type='button' className='w-100 mt-2' onClick= {() => navigate('/login')} >
-                SignIn
-               </Button>
+    <Container className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+    <Card className="w-100" style={{ maxWidth: '430px', boxShadow:'-11px 9px 5px 0px rgba(99,219,252,0.75)' , webkitBoxShadow:' -11px 9px 5px 0px rgba(99,219,252,0.75)',
+                               MozBoxShadow :'-11px 9px 5px 0px rgba(99,219,252,0.75)' }}>
+      <Card.Body>
+        {/*<h2 className="text-center mb-4" style={{color:'rgb(99,219,252)'}}>Sign Up</h2>*/}
+        <img src='src\Signup image.jpg' alt='Image' style={{width:'100%', border:'white'}}/>
+        <Form >
+          <Form.Group controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              
+            />
+          </Form.Group>
+          <Form.Group controlId="confirmpassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={cpassword}
+              onChange={(e) => setCpassword(e.target.value)}
+            />
+          </Form.Group>
+          <Button type="submit" className="w-100 mt-3" onClick={Submit}>
+            Sign Up
+          </Button>
+        </Form>
+        <div className="text-center mt-3">
+          Already Have an Account?
+          <br />
+          <Button type="button" variant="light" className="w-100 mt-3" onClick={() => navigate('/login')}>
+            Login
+          </Button>
         </div>
-        </>
+      </Card.Body>
+    </Card>
+  </Container>
+  </>
     )
-}
+
+    }
 
 
 export default SignUp;

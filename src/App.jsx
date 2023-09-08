92a327942eb544ce5c7d28aeab2f45ea0e1817f5
MohammedNayeemE <127304665+MohammedNayeemE@users.noreply.github.com>
2023-09-08 22:28:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import SignUp from "./signup";
 import Journal from "./journal";
-import { Container } from "react-bootstrap";
+//import { Container } from "react-bootstrap";
 import MoodTracker from "./mood";
 import Login from "./login.jsx";
 import {BrowserRouter as Router , Route , Routes} from 'react-router-dom';
@@ -15,6 +15,7 @@ function App(){
                 <Route path="/journal" element={<Journal/>}/>
                 <Route path="/mood" element={<MoodTracker/>}/>
                 <Route path="/login" element = {<Login/>}/>
+                <Route path ="/journal/login" element={<Login/>}/>
             </Routes>
         </Router>
         </>
@@ -24,10 +25,3 @@ function App(){
 export default App;
 
 
-/*
-<Container className="d-flex flex-column justify-content-center align-items-center" style={{ height: '100vh' }}>
-            <div className="w-100" style={{maxWidth:"430px"}}>
-            <SignUp/>
-            </div>
-        </Container>
-        */
